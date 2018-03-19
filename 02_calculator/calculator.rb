c289f34c60370083ba242eb3773040194c64f3de
Hanna van Leent <hanna.van.leent@gmail.com>
2018-03-19 14:02:35 +0100

    exercise 2 finished

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,40 @@
 #write your code here
+def add (a, b)
+    add = a + b
+    return add
+end
+
+def subtract(a, b)
+    subtract = a - b
+    return subtract
+end
+
+def sum array
+    tot = 0
+    array.each do |x|
+        tot += x
+    end
+    return tot 
+end
+
+def multiply array
+    tot = 1
+    array.each do |x|
+        tot *= x
+    end
+    return tot
+end
+
+def power(a, b)
+    power = a ** b
+    return power
+end
+
+def factorial(a)
+    sum = 1
+  until a == 0
+    sum *= a
+    a -= 1
+  end
+ sum
+end
